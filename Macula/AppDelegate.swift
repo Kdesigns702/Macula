af529b5a25dec9fcf8ca3553e7eef02d527e146e
Alexander Balobanov <alexander@balobanov.com>
2017-10-17 19:25:54 +0400

    The launch screen prototype added.

--- Macula/AppDelegate.swift
+++ Macula/AppDelegate.swift
@@ -14,12 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // the status bar is hidden on startup, let's make it visible
+		application.isStatusBarHidden = false
         return true
     }
-
+	
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
